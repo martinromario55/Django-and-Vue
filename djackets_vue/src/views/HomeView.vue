a97ae708863374cf0876eac4597fdb8672d3e8d1
martinromario55 <martinromario55@gmail.com>
2023-07-13 07:40:27 +0300

    initial commit

---
+++ djackets_vue/src/views/HomeView.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="home">
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">Welcome to Djackets</p>
+        <p class="subtitle">The best Fashion store online</p>
+      </div>
+    </section>
+
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">Latest products</h2>
+      </div>
+
+      <ProductBox 
+        v-for="product in latestProducts"
+        v-bind:key="product.id"
+        v-bind:product="product"
+      />
+    </div>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import ProductBox from '@/components/ProductBox.vue';
+
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      latestProducts: []
+    }
+  },
+  components: {
+    ProductBox
+  },
+  mounted() {
+    this.getLatestProduct()
+
+    // Set document title
+    document.title = 'Home | Djackets'
+  },
+  methods: {
+    async getLatestProduct() {
+      this.$store.commit('setIsLoading', true)
+     await axios
+        .get('/api/v1/latest-products/')
+        .then(response => {
+          this.latestProducts = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        }) 
+      this.$store.commit('setIsLoading', false)
+    }
+  },
+}
+</script>
